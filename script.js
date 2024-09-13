f85dea86383de6de6d656c31b67791418cccce2b
Rhibro <rhibron@gmail.com>
2024-09-13 15:49:25 +0200

    done I think

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-links").forEach(n => n.
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navLinks.classList.remove("active");
+    }))
